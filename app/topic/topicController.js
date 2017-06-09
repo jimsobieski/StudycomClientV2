aadb8c9c648f62fe20decb9731ceff411ef19be2
Sébastien Taton <Sbastien Taton>
2017-06-09 23:02:13 +0200

    design topic feed ok

--- app/topic/topicController.js
+++ app/topic/topicController.js
@@ -7,8 +7,26 @@ angular.module('myApp.topicController', ['ngRoute'])
 
         $scope.getTopicMessages = function () {
             var messages = [];
-            for(var i = 0; i< 15; i++) {
-                messages.push('message '+i);
+            for(var i = 1; i< 15; i++) {
+                if(i%2==0) {
+                    var message = {
+                        'idAuthor': 1,
+                        'author': 'Jim Sobieski',
+                        'text': 'lorem ipsum dolor sit amet'
+
+                    }
+                    messages.push(message);
+                }
+                else {
+                    var message = {
+                        'idAuthor': 2,
+                        'author': 'John Doe',
+                        'text': 'lorem ipsum dolor sit amet bla bla blabla ...'
+
+                    }
+                    messages.push(message);
+                }
+
             }
             $scope.messages = messages;
         }
@@ -16,8 +34,39 @@ angular.module('myApp.topicController', ['ngRoute'])
 
 
         $scope.addMessage = function () {
-            $scope.messages.push($scope.message);
+            var message = {
+                'idAuthor': 1,
+                'author': 'Jim Sobieski',
+                'text': $scope.message
+
+            }
+            $scope.messages.push(message);
             $scope.message = '';
+            scrollBottom();
+        }
+
+        $scope.leftOrRight = function (message) {
+            if (message.idAuthor==1) {
+                return 'end center';
+            }
+            else {
+                return 'start center';
+            }
+        }
+        $scope.authMessage = function (message) {
+            if (message.idAuthor==1) {
+                return 'topic-auth-message';
+            }
+            else {
+                return '';
+            }
+        }
+
+        var scrollBottom = function () {
+            var messages = document.getElementsByClassName('topic-message-card');
+            console.log(messages[messages.length-1]);
+            messages[messages.length-1].scrollTop = messages[messages.length-1].scrollHeight;
+
         }
 
     });
