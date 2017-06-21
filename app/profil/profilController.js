8484c0b6192dd4b11ee40446ca55490565ddb612
SaherSL <saher.elgoff@hotmail.fr>
2017-06-22 00:22:25 +0200

    profile card create

--- app/profil/profilController.js
+++ app/profil/profilController.js
@@ -2,6 +2,17 @@
 
 angular.module('myApp.profilController', ['ngRoute'])
 
-.controller('profilController', [function() {
+.controller('profilController', function($scope, Auth) {
 
-}]);
+    $scope.userEdit = false;
+
+    Auth.user().then(function(response) {
+        $scope.user = response;
+        $scope.userToEdit = angular.copy($scope.user);
+        console.log($scope.userToEdit);
+    });
+
+    $scope.editProfile = function () {
+        $scope.userEdit = !$scope.userEdit;
+    }
+});
